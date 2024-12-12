4a5fda9d25ef6ba206f4f2a16c14c2f2b7f12cc1
BugHunter-X <askzenai@gmail.com>
2024-12-12 16:44:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This React Native code uses the deprecated `componentWillMount` lifecycle method, which can lead to unexpected behavior and errors.  It should be replaced with `componentDidMount` or a functional component with useEffect hook.
